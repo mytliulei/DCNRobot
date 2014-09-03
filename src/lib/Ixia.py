22fce3faca6cbbb0616285e206f82b5ccc37e43c
liulei <mytliulei@gmail.com>
2014-09-03 16:56:47 +0800

    add ixia local keyword lib

---
+++ src/lib/Ixia.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+'''ixia stream genertor Tools,for RF testing 
+'''
+import os,os.path
+import inspect
+
+from robot.api import logger
+from robot.version import get_version
+from scapy.all import *
+
+import rfbase
+
+__version__ = '0.1'
+__author__ = 'liuleic'
+__copyright__ = 'Copyright 2014, DigitalChina Network'
+__license__ = 'Apache License, Version 2.0'
+__mail__ = 'liuleic@digitalchina.com'
+
+class Ixia(object):
+    '''
+    '''
+    ROBOT_LIBRARY_SCOPE = 'TEST_SUITE'
+    ROBOT_LIBRARY_VERSION = get_version()
+    def __init__(self):
+        ''''''
+        self._pkt_streamlist_hexstring = []
+        self._pkt_kws = self._lib_kws = None
+        self._pkt_class = rfbase.PacketBase()
+
+    def get_keyword_names(self):
+        return self._get_library_keywords() + self._get_pkt_keywords()
+
+    def _get_library_keywords(self):
+        if self._lib_kws is None:
+            self._lib_kws = self._get_keywords(self, ['get_keyword_names'])
+        return self._lib_kws
+
+    def _get_keywords(self, source, excluded):
+        return [name for name in dir(source)
+                if self._is_keyword(name, source, excluded)]
+
+    def _is_keyword(self, name, source, excluded):
+        return (name not in excluded and
+                not name.startswith('_') and
+                name != 'get_keyword_names' and
+                inspect.ismethod(getattr(source, name)))
+
+    def _get_pkt_keywords(self):
+        if self._pkt_kws is None:
+            pkt = self._pkt_class
+            excluded = ['get_packet_list','empty_packet_list']
+            self._pkt_kws = self._get_keywords(pkt, excluded)
+        return self._pkt_kws
+
+    def __getattr__(self, name):
+        if name not in self._get_pkt_keywords():
+            raise AttributeError(name)
+        # This makes it possible for Robot to create keyword
+        # handlers when it imports the library.
+        return getattr(self._pkt_class, name)
+
+    def get_stream_from_pcapfile(self,filename):
+        '''read pcap file and return bytes stream'''
+        if not os.path.isfile(filename):
+            logger.info('%s is not a file' % filename)
+            raise AssertionError('%s is not file or path error' % filename)
+        with open(filename,'rb') as handle:
+            return handle.read()
+
+    def build_stream(self,alias):
+        ''''''
+        self._pkt_streamlist_hexstring = self._pkt_class.get_packet_list()
+        self._pkt_class.empty_packet_list()
+        return self._pkt_streamlist_hexstring
+
+    def start_proxy_server(self,alias,ixos_ver):
+        '''
+        '''
+        pass
+
+    def close_proxy_server(self,alias):
+        '''
+        '''
+        pass
+
+    def _is_proxyserver_live(self):
+        '''
+        '''
+        pass
+
+    
