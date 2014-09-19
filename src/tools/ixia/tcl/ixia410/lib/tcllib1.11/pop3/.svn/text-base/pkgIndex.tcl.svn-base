1dfbe698c45182811ac6c95e7b8c5b3013ef719b
liulei <mytliulei@gmail.com>
2014-09-19 09:30:23 +0800

    DEL 删除ixia解释器文件

--- src/tools/ixia/tcl/ixia410/lib/tcllib1.11/pop3/.svn/text-base/pkgIndex.tcl.svn-base
+++
@@ -1,2 +0,0 @@
-if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded pop3 1.7 [list source [file join $dir pop3.tcl]]
--- src/tools/ixia/tcl/ixia410/lib/tcllib1.11/snit/pkgIndex.tcl
+++
@@ -1,6 +0,0 @@
-if {[package vsatisfies [package provide Tcl] 8.5]} {
-    package ifneeded snit 2.2.3 \
-        [list source [file join $dir snit2.tcl]]
-}
-
-package ifneeded snit 1.3.3 [list source [file join $dir snit.tcl]]
